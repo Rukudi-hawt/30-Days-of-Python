a90326cca44e580ca3adf2bd0b486dd84020a177
Rukudzo Dube <rukudihotasf@gmail.com>
2026-08-04 01:40:26 +0200

    Finished first part of level 1 exercises

--- Day 12.py
+++ Day 12.py
@@ -3,7 +3,17 @@
 # help(math)
 
 import random 
+import string
 
-def random_user_id(user_id):
-     user_id += randint(0, 9)
-     
+def random_user_id():
+     user_id = ''
+     user_id += str(random.randint(0, 9))
+     user_id += random.choice(string.ascii_lowercase)
+     user_id += random.choice(string.ascii_lowercase)
+     user_id += str(random.randint(0, 9))
+     user_id += str(random.randint(0, 9))
+     user_id += random.choice(string.ascii_lowercase)
+
+     return user_id
+
+print(random_user_id())
