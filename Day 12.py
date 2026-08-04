e89ff770577dfec195fe08e9d9a3087fb707ad9b
Rukudzo Dube <rukudihotasf@gmail.com>
2026-08-04 03:18:47 +0200

    Completed question 2 of level 1 exercises

--- Day 12.py
+++ Day 12.py
@@ -16,4 +16,30 @@ def random_user_id():
 
      return user_id
 
-print(random_user_id())
+#print(random_user_id())
+
+def user_id_gen_by_user():
+     length = int(input('Number of charaacters:', ))
+     tally = int(input('Number of codes:', ))
+     #codes = []
+     character = 0
+     code = ''
+
+     for number_of_codes in range(1, tally + 1, 1):
+          code = ''
+
+          for number_of_characters in range(1, length + 1, 1):
+               character = random.randint(0,1)
+
+               if character == 0:
+                    code += str(random.randint(0,9))
+
+               if character == 1:
+                    code += random.choice(string.ascii_letters)
+
+          #codes.append(code)
+          print(code)
+     #return codes
+
+user_id_gen_by_user()
+
