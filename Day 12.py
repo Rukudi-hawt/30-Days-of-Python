83410934d022f30d6b73e2c25818ba09aa434f28
Rukudzo Dube <rukudihotasf@gmail.com>
2026-08-04 18:49:50 +0200

    Completed level 2 exercises.

--- Day 12.py
+++ Day 12.py
@@ -41,5 +41,115 @@ def user_id_gen_by_user():
           print(code)
      #return codes
 
-user_id_gen_by_user()
+#user_id_gen_by_user()
 
+def rgb_color_gen():
+     red = random.randint(0,255)
+     green = random.randint(0,255)
+     blue = random.randint(0,255)
+
+     rgb = 'rgb('+ str(red) + ',' + str(green) + ','+ str(blue) + ')'
+     return rgb
+
+#print(rgb_color_gen())
+
+def rgb_color_gen():
+    red = random.randint(0, 255)
+    green = random.randint(0, 255)
+    blue = random.randint(0, 255)
+
+    return f"rgb({red}, {green}, {blue})"
+
+#print(rgb_color_gen())
+
+def list_of_hexa_colors(length):
+     hexa_list = []
+     hex_code = '#'
+     character = 0
+
+     for lst in range(1, length + 1, 1): #correlates with given amount of codes
+          hex_code = '#'
+
+          for code in range(1, 7, 1): #creates code; 7-code length
+               character = random.randint(0, 1)
+
+               if character == 0:
+                    hex_code += str(random.randint(0, 9))
+
+               if character == 1:
+                    hex_code += random.choice(string.ascii_lowercase)
+
+          hexa_list.append(hex_code)
+
+     return hexa_list
+
+#print(list_of_hexa_colors(19))
+
+def list_of_rgb_colors(length):
+     rgb_list = []
+     rgb = ''
+
+     for lst in range(0, length):
+          rgb = ''
+
+          red = random.randint(0, 255)
+          green = random.randint(0, 255)
+          blue = random.randint(0, 255)
+
+          rgb = f"rgb({red}, {green}, {blue})"
+          rgb_list.append(rgb)
+     return rgb_list
+
+#print(list_of_rgb_colors(9))
+
+def generate_colors(type, length):
+     hexa_list = []
+     hex_code = '#'
+     character = 0
+
+     rgb_list = []
+     rgb = ''
+
+     colour_list = []
+
+     if type.lower() == 'hexa':
+          for lst in range(1, length + 1, 1): #correlates with given amount of codes
+                    hex_code = '#'
+          
+                    for code in range(1, 7, 1): #creates code; 7-code length
+                         character = random.randint(0, 1)
+          
+                         if character == 0:
+                              hex_code += str(random.randint(0, 9))
+          
+                         if character == 1:
+                              hex_code += random.choice(string.ascii_lowercase)
+          
+                    hexa_list.append(hex_code)
+          return hexa_list
+
+
+     if type.lower() == 'rgb':
+
+          for lst in range(0, length):
+                    rgb = ''
+          
+                    red = random.randint(0, 255)
+                    green = random.randint(0, 255)
+                    blue = random.randint(0, 255)
+          
+                    rgb = f"rgb({red}, {green}, {blue})"
+                    rgb_list.append(rgb)
+          return rgb_list
+
+     # if len(rgb_list) > 0:
+     #      colour_list.append(rgb_list)
+     #      return rgb_list
+     # else:
+     #      colour_list.append(hexa_list)
+     #      return hexa_list
+
+     #return colour_list
+
+print(generate_colors('rgb', 3))
+     
