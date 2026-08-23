abc7e8cbffc00aa38f89198a2fe699e565e5e3b6
Rukudzo Dube <rukudihotasf@gmail.com>
2026-08-23 02:22:07 +0200

    More of day 14 done.

--- Day 14.py
+++ Day 14.py
@@ -1,4 +1,5 @@
 import math
+import functools
 
 def sum_numbers(nums):  # normal function
     return sum(nums) 
@@ -47,3 +48,44 @@ def is_six_lettered(country):
 
 non_six_lettered = filter(is_six_lettered, countries)
 #print(list(non_six_lettered))
+
+def is_six_lettered_or_more(country):
+    if len(country) >= 6:  
+        return False
+    return True
+
+non_six_lettered_or_more = filter(is_six_lettered_or_more, countries)
+#print(list(non_six_lettered_or_more))
+
+def has_E(country):
+    if 'E' in country:  
+        return False
+    return True
+
+non_has_E = filter(has_E, countries)
+#print(list(non_has_E))
+
+upper_non_E = map(upper_case, (filter(has_E, names)))
+#print(list(upper_non_E))
+
+def get_string_lists(lst):
+    new_list = []
+    for i in lst:
+        new_list.append(str(i))
+    return new_list
+
+#print(get_string_lists(numbers))
+
+def sum_two_numbers(x, y):
+    return int(x) + int(y)
+
+reduced_list = functools.reduce(sum_two_numbers, numbers)
+#print(reduced_list)
+
+def concatenate_countries(country1, country2):
+    if country2 == countries[len(countries) - 1]:
+        return country1 + ', and ' + country2
+    return country1 + ', ' + country2
+
+concatenated_countries = functools.reduce(concatenate_countries, countries)
+print(concatenated_countries + ' are all countries in Europe.')
